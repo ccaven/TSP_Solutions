b8dff966fa1e117073fecb6e79b147493e6d1182
ccaven <chacecaven@outlook.com>
2016-11-24 14:27:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ import Genetic_Algorithm
 from simAnneal import anneal
 from simAnneal import visualize_tsp
 end_distances = {"sort by x":0,"sort by y":0,"next nearest":0,"brute force":0,"ga":0,"sa":0}
-for i in range(10):
-    
+num_trials = 10
+for i in range(num_trials):
+    #Test comment
